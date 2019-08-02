0e94df90493f9fe54a50f67dd21d513b5668d7b3
suneqing <suneqing@163.com>
2019-08-02 10:06:33 +0800

    整合Elasticsearch

--- src/main/java/com/ray/repository/SkuRepository.java
+++ src/main/java/com/ray/repository/SkuRepository.java
@@ -3,6 +3,16 @@ package com.ray.repository;
 import com.ray.model.Sku;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface SkuRepository extends ElasticsearchRepository<Sku,Long> {
+
+    /**
+     * 查询价格范围
+     * @param startPrice
+     * @param endPrice
+     * @return
+     */
+    List<Sku> findByPriceBetween(double startPrice, double endPrice);
 
 }
--- src/test/java/com/ray/test/ElasticsearchApplicationTests.java
+++ src/test/java/com/ray/test/ElasticsearchApplicationTests.java
@@ -1,14 +1,19 @@
 package com.ray.test;
 
+import com.alibaba.fastjson.JSON;
 import com.ray.model.Sku;
 import com.ray.repository.SkuRepository;
 import org.assertj.core.util.Lists;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -61,5 +66,50 @@ public class ElasticsearchApplicationTests {
         Iterable<Sku> list = this.skuRepository.findAll(Sort.by("id").ascending());
     }
 
+    @Test
+    public void findByPriceBetween(){
+        List<Sku> list = skuRepository.findByPriceBetween(0,1000);
+        System.out.printf(JSON.toJSONString(list));
+    }
+
+    /**
+     * 自定义查询
+     */
+    @Test
+    public void testMatchQuery(){
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        queryBuilder.withQuery(QueryBuilders.matchQuery("tital","小米手机"));
+        Page<Sku> items = skuRepository.search(queryBuilder.build());
+        System.out.printf(JSON.toJSONString(items));
+    }
+
+
+    @Test
+    public void searchByPage(){
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 添加基本分词查询
+        queryBuilder.withQuery(QueryBuilders.termQuery("category", "手机"));
+        // 分页：
+        int page = 0;
+        int size = 2;
+        queryBuilder.withPageable(PageRequest.of(page,size));
+
+        // 搜索，获取结果
+        Page<Sku> items = this.skuRepository.search(queryBuilder.build());
+        // 总条数
+        long total = items.getTotalElements();
+        System.out.println("总条数 = " + total);
+        // 总页数
+        System.out.println("总页数 = " + items.getTotalPages());
+        // 当前页
+        System.out.println("当前页：" + items.getNumber());
+        // 每页大小
+        System.out.println("每页大小：" + items.getSize());
+
+        for (Sku item : items) {
+            System.out.println(item);
+        }
+    }
 
 }
